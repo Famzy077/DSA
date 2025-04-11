66fdda3ee07198996e1e4526f4e80af4122f47de
Akinola Femi <akinolafemi573@gmail.com>
2025-04-11 07:02:25 +0100

    Array basics

---
+++ DSA/Array.js
@@ -0,0 +1,20 @@
+// Find the minimum number in an array
+let indexs = [10, 13, 5, 19, 2, 7, 3]
+let minIndex = indexs[0];
+for(let e in indexs){
+    if(e < minIndex){
+        minIndex = e;
+    }
+}
+console.log('Lowest value: ' + minIndex);
+
+let checkLowest = [10, 13, 5, 19, 2, 7, 3];
+let minValue = checkLowest[0];
+
+for (let e of checkLowest) {
+  if (e < minValue) {
+    minValue = e;
+  }
+}
+
+console.log('Lowest value: ' + minValue);
